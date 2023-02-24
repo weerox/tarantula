01d5c867c9b9c3f1ca099e676fdb00e2b45fa1d4
Aron Bergman <bathingrad@gmail.com>
2023-02-24 23:47:58 +0100

    Convert to tab indentation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Command;
 
 fn main() {
-    let cli = Command::new("cargo-tarantula");
+	let cli = Command::new("cargo-tarantula");
 
-    let matches = cli.get_matches();
+	let matches = cli.get_matches();
 }
