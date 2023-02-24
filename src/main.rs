af3a5e747c89c93fd3aa4de60bed11309bce8ffd
Aron Bergman <bathingrad@gmail.com>
2023-02-24 23:58:37 +0100

    Add CLI arg for manifest path

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use clap::Command;
+use clap::{Command, Arg};
 
 fn main() {
-	let cli = Command::new("cargo-tarantula");
+	let cli = Command::new("cargo-tarantula")
+		.arg(Arg::new("manifest-path").long("manifest-path").help("Path to Cargo.toml"));
 
 	let matches = cli.get_matches();
+
+	let manifest_path: Option<&String> = matches.get_one("manifest-path");
 }
